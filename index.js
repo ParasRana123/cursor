d26593ba589e9c82ea624392af3b37c1995e2c8f
ParasRana123 <parasrana579@gmail.com>
2025-06-28 15:33:20 +0530

    Added a test function and passed it as a tool to the system prompt

--- index.js
+++ index.js
@@ -4,10 +4,28 @@ dotenv.config();
 
 const ai = new GoogleGenAI(process.env.GEMINI_API_KEY);
 
+function addTwoNumbers(x , y) {
+  return x + y;
+}
+
+const SYSTEM_PROMPT = `
+  You are a helpfull AI Assistant who is designed to resolve user query.
+  If you think, user query needs a tool invocation , just tell me tool
+  name with parameters.
+
+  Available Tools: 
+  - addTwoNumbers(x: number , y: number): Returns Number
+`
+
 async function main() {
   const response = await ai.models.generateContent({
     model: "gemini-2.5-flash",
-    contents: "Explain what is machine learning",
+    contents:  [
+      {
+        role: "user",
+        parts: [{ text: SYSTEM_PROMPT + "What is 5 + 9" }]
+      }
+    ],
   });
   console.log(response.text);
 }
